ddf68043e7e8ae2422fa2fcb1d31f7b1e5cf2452
Adam Sahraoui <adamsahraoui@MacBook-Pro-de-Adam.local>
2020-11-03 16:37:40 +0100

    exo7

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+
+function divideThem(){ 
+var numberOne= document.getElementById("numberOne").value;
+var numberTwo= document.getElementById("numberTwo").value;
+var result= (numberOne % numberTwo);
+return result;
+}
+
+
+
+
+function displayAlert(){ 
+      var myResult = divideThem();
+      alert(myResult);
+  }
+  
+
+
+
+
+
+function sendData(){ 
+
+    var pointure= document.getElementById("pointure").value;
+    var naissance= document.getElementById("naissance").value; 
+    resultat = (((((pointure * 2) + 5)* 50) - naissance) + 1769) ;
+    
+    alert("Le resultat est : " + resultat);
+  }
+
+
+function age() {
+    var old= document.getElementById("old").value;
+
+    if(old>18)
+    alert("Vous êtes majeur");
+
+    if(old<18)
+    alert("Vous êtes mineur");
+
+
+}
