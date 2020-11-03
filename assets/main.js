7a23738e210632989ccc0b69ba197cb98aac4293
Adam Sahraoui <adamsahraoui@MacBook-Pro-de-Adam.local>
2020-11-03 12:06:06 +0100

    exo6

--- assets/main.js
+++ assets/main.js
@@ -4,3 +4,13 @@ var x = 5;
 var y = 2;
 alert(x % y);
 }
+
+
+function sendData(){ 
+
+    var pointure= document.getElementById("pointure").value;
+    var naissance= document.getElementById("naissance").value; 
+    resultat = (pointure * 2 + 5)* 50 - naissance + 1769 ;
+    
+    alert("Le resultat est : " + resultat);
+  }
